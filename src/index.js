722cc794ce44e57565a02285d90b29c37e422792
Luis Isasi <luisi@oktana.io>
2019-10-10 07:32:06 -0300

    spinner component, render conditional components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 
 class App extends React.Component {
@@ -23,19 +24,25 @@ class App extends React.Component {
     //     console.log('my component was just updated - it rerendered')
     // }
 
-    render() {
-
-
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
+            return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat}/>
+            return <SeasonDisplay lat={this.state.lat}/>;
         }
 
-        return <div>Loading...</div>
+        return <Spinner message="Please accept location request"/>;
+    }
 
+    render() {
+
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
